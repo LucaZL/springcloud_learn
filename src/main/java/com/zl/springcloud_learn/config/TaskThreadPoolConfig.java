eacecd82bb0e0d1fa4d01d934062e1ba6391d036
Lu Zhao <luzhao0229@gmail.com>
2019-12-27 00:58:20 +0800

    6.5 异步线程和线程池

---
+++ src/main/java/com/zl/springcloud_learn/config/TaskThreadPoolConfig.java
@@ -0,0 +1,17 @@
+package com.zl.springcloud_learn.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.task.pool")
+@Data
+public class TaskThreadPoolConfig {
+    private int corePoolSize = 5;
+    private int maxPoolSize = 50;
+    private int keepAliveSeconds = 60;
+    private int queueCapacity = 1000;
+    private String threadNamePrefix = "FSH-AsyncTask";
+
+}
--- src/main/java/com/zl/springcloud_learn/service/async/AsyncMyServiceImpl.java
+++ src/main/java/com/zl/springcloud_learn/service/async/AsyncMyServiceImpl.java
@@ -1,7 +1,10 @@
 package com.zl.springcloud_learn.service.async;
 
+import com.sun.org.apache.xerces.internal.dom.AbortException;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
+
+import java.lang.reflect.UndeclaredThrowableException;
 
 @Service
 public class AsyncMyServiceImpl implements AsyncMyService {
@@ -9,6 +12,7 @@ public class AsyncMyServiceImpl implements AsyncMyService {
     public void continuePrintNumber() {
         for(int i = 0; i < 20; i++) {
             System.out.println(i);
+            if(i > 10) throw new AbortException();
         }
     }
 }
