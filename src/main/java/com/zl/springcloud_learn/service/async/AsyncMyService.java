7bbc80938f2b5f1f0d50a51880d6f82a9a96c705
Lu Zhao <luzhao0229@gmail.com>
2019-12-27 00:06:27 +0800

    6.5 异步线程执行

--- src/main/java/com/zl/springcloud_learn/service/async/AsyncMyService.java
+++ src/main/java/com/zl/springcloud_learn/service/async/AsyncMyService.java
@@ -2,6 +2,7 @@ package com.zl.springcloud_learn.service.async;
 
 import org.springframework.scheduling.annotation.Async;
 
+@Async
 public interface AsyncMyService {
     void continuePrintNumber();
 }
--- src/main/java/com/zl/springcloud_learn/service/async/AsyncMyServiceImpl.java
+++ src/main/java/com/zl/springcloud_learn/service/async/AsyncMyServiceImpl.java
@@ -4,11 +4,10 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
 
 @Service
-@EnableAsync
 public class AsyncMyServiceImpl implements AsyncMyService {
     @Override
     public void continuePrintNumber() {
-        for(int i = 0; i < 1000; i++) {
+        for(int i = 0; i < 20; i++) {
             System.out.println(i);
         }
     }
